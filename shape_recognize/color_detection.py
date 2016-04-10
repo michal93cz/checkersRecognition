bc74a42a427e79714560b5392746456705155280
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2016-04-10 15:30:04 +0200

    color detection test

---
+++ shape_recognize/color_detection.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while(1):
+
+    # Take each frame
+    _, frame = cap.read()
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # define range of blue color in HSV
+    lower_blue = np.array([110,50,50])
+    upper_blue = np.array([130,255,255])
+
+    # zakres brany z http://www.rapidtables.com/web/color/RGB_Color.htm
+    # fiolet
+    lower = np.array([153,0,78])
+    upper = np.array([255,204,229])
+    # Threshold the HSV image to get only blue colors
+    # mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask = cv2.inRange(hsv, lower, upper)
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
