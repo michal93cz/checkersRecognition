0d64562ece567a946b5e3e09e0bba312496878e7
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2016-04-10 14:30:42 +0200

    saving/reading natural color image to/from stream

--- shape_recognize/read_write_cv2_stream_to_jpg.py
+++ shape_recognize/read_write_cv2_stream_to_jpg.py
@@ -9,22 +9,15 @@ cap = cv2.VideoCapture(0)
 
 while (True):
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    frame = cap.read()
 
-    # frame = cv2.cvtColor(frame, cv2.COLORMAP_AUTUMN)
-    frame = cv2.Canny(frame, 280, 350)
-    normal = cv2.cvtColor(frame, cv2.COLOR_BAYER_GB2BGR)
-    # laplacian = cv2.Laplacian(frame,cv2.CV_64F)
+    normal = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    #cv2.imshow('frame',laplacian)
-    #cv2.imshow('frame', normal)
-    #cv2.imshow('frame')
     # zapis do pliku
     cv2.imwrite('img_CV2_90.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
 
     frame = cv2.imread('img_CV2_90.jpg')
-    cv2.imshow('frame', normal)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
