98540685fa3e964deb3c1bb62042f15f6790744a
Michał Czupryniak <michal.czupryniak@gmail.com>
2016-04-26 22:54:19 +0200

    Checkers module using and refactoring

--- checkersLogic/main.py
+++ checkersLogic/main.py
@@ -1,4 +1,4 @@
-import checkersRecognition.checkersLib.checkers
+from checkersLib.checkers import CheckersInterface
 from enum import Enum
 
 class PIECE(Enum):
@@ -27,3 +27,6 @@ def move_white_paws(type, fromWhere, toWhere):
     if BOARD_ARRAY[toWhere[0]][toWhere[1]] != 0:
         BOARD_ARRAY[toWhere[0]][toWhere[1]] = type
         BOARD_ARRAY[fromWhere[0]][fromWhere[1]] = 0
+
+CI = CheckersInterface()
+CI.master.mainloop()
